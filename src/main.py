7e50d74efc160c41c679078d2d00ad8474ceab23
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-03-30 22:53:24 +0530

    Implemented online classifier

--- src/main.py
+++ src/main.py
@@ -63,7 +63,7 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
         plt.yticks(())
     plt.show()
 
-class eF:
+class model:
     def __init__(self,Data):
         self.load(Data)
 
@@ -72,12 +72,12 @@ class eF:
         data_set_labels = []
         for person in Data.males:
             data_set.append(person.flatten())
-            data_set_labels.append(1)
+            data_set_labels.append('male')
         for person in Data.females:
             data_set.append(person.flatten())
-            data_set_labels.append(-1)
+            data_set_labels.append('female')
         h,w=person.shape
-        X_train, X_test, y_train, y_test = train_test_split(data_set, data_set_labels, test_size=0.01, random_state=datetime.now().second)
+        X_train, X_test, y_train, y_test = train_test_split(data_set, data_set_labels, test_size=0.1, random_state=datetime.now().second)
 
         n_components = 15
 
@@ -89,8 +89,59 @@ class eF:
         #plot_gallery(eigenfaces, eigenface_titles, h, w)
         self.clf = LinearSVC()
         self.clf.fit(X_train_pca,y_train)
+        print X_test_pca[0].shape
         s=self.clf.score(X_test_pca,y_test)
         print s
 
+class video:
+    def __init__(self,model):
+        self.load(model)
+
+    def load(self,model):
+        cap = cv2.VideoCapture(0)
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+        pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+        while True:
+            flag, frame = cap.read()
+            if flag:
+                # The frame is ready and already captured
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                for (x,y,w,h) in faces:
+                    face = gray[y:y+h, x:x+w]
+                    dim=cv2.resize(face, (100, 100), interpolation = cv2.INTER_AREA)
+                    dim = np.array(dim)
+                    dim=dim.flatten()
+                    print dim
+                    transform = model.pca.transform([dim])[0]
+                    print transform.shape
+                    prediction = model.clf.predict([transform])[0]
+                    # print prediction
+                    if prediction == 'male':
+                        colo = (255)
+                        print 'male'
+                    else:
+                        colo = (0)
+                        print "female"
+                    cv2.rectangle(gray,(x,y),(x+w,y+h),colo,2)
+                cv2.imshow('video', gray)
+                pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                print str(pos_frame)+" frames"
+            else:
+                # The next frame is not ready, so we try to read it again
+                cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame-1)
+                print "frame is not ready"
+                # It is better to wait for a while for the next frame to be ready
+                cv2.waitKey(1000)
+
+            if cv2.waitKey(10) == 27:
+                break
+            if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                # If the number of captured frames is equal to the total number of frames,
+                # we stop
+                break
+
+
 a=Data()
-b=eF(a)
+b=model(a)
+c=video(b)
