e1eb1e5fd4393e30c021d7946000fb2e921f887f
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-06 19:36:27 +0530

    Changes in Plot Gallery

--- src/video.py
+++ src/video.py
@@ -51,15 +51,3 @@ class video:
             #     # If the number of captured frames is equal to the total number of frames,
             #     # we stop
             #     break
-
-def plot_gallery(images, titles, h, w, n_row=5, n_col=15):
-    """Helper function to plot a gallery of portraits"""
-    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_row * n_col):
-        plt.subplot(n_row, n_col, i + 1)
-        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-        plt.title(titles[i], size=12)
-        plt.xticks(())
-        plt.yticks(())
-    plt.show()
